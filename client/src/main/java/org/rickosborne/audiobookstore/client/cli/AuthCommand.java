42f90992230a07f9d427d613235047ae8c6eca7f
Rick Osborne <ricko@rickosborne.org>
2022-06-18 08:43:58 -0700

    Better `auth` success test and message

--- client/src/main/java/org/rickosborne/audiobookstore/client/cli/AuthCommand.java
+++ client/src/main/java/org/rickosborne/audiobookstore/client/cli/AuthCommand.java
@@ -1,16 +1,26 @@
 package org.rickosborne.audiobookstore.client.cli;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.DimensionRange;
+import com.google.api.services.sheets.v4.model.GridData;
+import com.google.api.services.sheets.v4.model.GridProperties;
+import com.google.api.services.sheets.v4.model.Sheet;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import lombok.extern.java.Log;
-import org.rickosborne.audiobookstore.client.google.Google;
 import picocli.CommandLine;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.List;
 import java.util.concurrent.Callable;
+
+import static org.rickosborne.audiobookstore.Sheets.BOOKS_SHEET_TITLE;
+import static org.rickosborne.audiobookstore.Sheets.SHEET_ID;
+import static org.rickosborne.audiobookstore.Sheets.booksSheet;
+import static org.rickosborne.audiobookstore.Sheets.getSpreadsheet;
+import static org.rickosborne.audiobookstore.Sheets.getSpreadsheets;
+import static org.rickosborne.audiobookstore.Sheets.sheetValues;
 
 @Log
 @CommandLine.Command(
@@ -18,24 +28,25 @@ import java.util.concurrent.Callable;
     description = "Authorize and save credentials"
 )
 public class AuthCommand implements Callable<Integer> {
-    @CommandLine.Parameters
+    @CommandLine.Parameters(paramLabel = "USERID", description = "Google user ID, generally an email address")
     private String userId;
 
     @Override
     public Integer call() throws GeneralSecurityException, IOException {
-        final Credential credential = Google.getCredentials(userId);
-        if (credential == null) {
-            log.warning("No Google credentials");
-            return 2;
+        if (userId == null || userId.isBlank()) {
+            throw new IllegalArgumentException("Expected parameter: userId");
         }
-        final Sheets sheets = new Sheets.Builder(
-            GoogleNetHttpTransport.newTrustedTransport(),
-            GsonFactory.getDefaultInstance(),
-            credential
-        )
-            .setApplicationName(getClass().getPackageName())
-            .build();
-        log.info("Got a Sheets client!  Check your credentials file.");
+        final Sheets.Spreadsheets spreadsheets = getSpreadsheets(userId);
+        final Spreadsheet spreadsheet = getSpreadsheet(spreadsheets);
+        if (spreadsheet == null || spreadsheet.isEmpty()) {
+            throw new IllegalStateException("Could not find, open, or retrieve Sheet.");
+        }
+        final Sheet books = booksSheet(spreadsheet);
+        final GridProperties gridProperties = books.getProperties().getGridProperties();
+        final Integer frozenRowCount = gridProperties.getFrozenRowCount();
+        final List<List<Object>> booksValues = sheetValues(BOOKS_SHEET_TITLE, spreadsheets);
+        final int bookCount = booksValues.size() - frozenRowCount;
+        System.out.println("Got access to \"" + spreadsheet.getProperties().getTitle() + "\" with " + bookCount + " books.");
         return 0;
     }
 }
